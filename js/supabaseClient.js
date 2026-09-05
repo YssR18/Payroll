2268804028e59a9e1c6ff15d123651e9710ae98a
YssR18 <sooy910@gmail.com>
2026-09-05 12:56:22 +0200

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 // ============================================================
 // إعداد الاتصال بـ Supabase
 // ============================================================
-const SUPABASE_URL = 'ضع_رابط_مشروعك_هنا';
-const SUPABASE_ANON_KEY = 'ضع_مفتاح_anon_هنا';
+const SUPABASE_URL = 'https://jvpthhuxipxomgqootzg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2cHRoaHV4aXB4b21ncW9vdHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NjEzNDgsImV4cCI6MjEwNDEzNzM0OH0.GY5qMGJrPCYmStuPAUTnYVZiDnmtLAKpy36ccXvdLs4';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
